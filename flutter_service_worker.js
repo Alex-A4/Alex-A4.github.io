1b8ea23f2db042142ee0cb898f3db3862aa06282
= <alex.a4.25@yandex.ru>
2020-04-29 20:03:20 +0300

    Fix calculating

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "fdecc1327f005d74c6395e267a6b3247",
 "/": "fdecc1327f005d74c6395e267a6b3247",
-"main.dart.js": "36bbb94a6f3b01e30a400eb4e5bb477e",
+"main.dart.js": "4fe1dc332c262ce4b52487b7bb0efb1b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
